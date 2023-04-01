494440298ac638600b63b0b15ee30ca9ebddbcd8
ErickChen <chenht1996@gmail.com>
2023-04-01 22:56:23 +0800

    add more plugs

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
 require('packer').startup(function()
-    use {'tpope/vim-fugitive'}
+    use {'airblade/vim-gitgutter'}
+    use {'dense-analysis/ale'}
+    use {'junegunn/fzf.vim'}
     use {'neoclide/coc.nvim', branch = 'release'}
+    use {'scrooloose/nerdtree'}
+    use {'tpope/vim-commentary'}
+    use {'tpope/vim-fugitive'}
+    use {'tpope/vim-surround'}
+    use {'vim-airline/vim-airline'}
+    use {'vim-airline/vim-airline-themes'}
     use {'wbthomason/packer.nvim'}
 end)
 
