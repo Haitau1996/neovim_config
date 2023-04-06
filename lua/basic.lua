1121875ea319c7faee4973988c08f3acc2647392
ErickChen <chenht1996@gmail.com>
2023-04-06 23:36:59 +0800

    add tab setting

--- lua/basic.lua
+++ lua/basic.lua
@@ -7,3 +7,12 @@ vim.o.sidescrolloff = 8
 -- 使用相对行号
 vim.wo.number = true
 vim.wo.relativenumber = true
+-- 启用 powerline
+vim.g.airline_powerline_fonts = 1
+-- 启用鼠标支持
+vim.o.mouse = 'a'
+-- 设置 tab 大小
+vim.opt.tabstop = 4
+vim.opt.shiftwith = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
