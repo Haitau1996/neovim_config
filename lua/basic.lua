92c424d5ae0607c40b1a1ee39bbf5017b21b0695
ErickChen <chenht1996@gmail.com>
2023-07-24 00:41:43 +0800

    feat: add tab for coc.nvim

--- lua/basic.lua
+++ lua/basic.lua
@@ -26,3 +26,6 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
 vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
 vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
 vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.cmd(
+   [[inoremap <silent><expr> <TAB> coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<TAB>"]]
+)
